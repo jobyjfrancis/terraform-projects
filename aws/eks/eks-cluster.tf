9c14e0ea1228c0e537960de365f0b37e32a79b4b
Joby Francis <jobyjfrancis@gmail.com>
2026-03-08 20:12:45 +1300

    eks

---
+++ aws/eks/eks-cluster.tf
@@ -0,0 +1,28 @@
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "20.24.0"
+
+  cluster_name = "myapp-eks-cluster"
+  cluster_version = "1.30"
+  cluster_endpoint_public_access = "true"
+
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id     = module.myapp-vpc.vpc_id
+
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 3
+
+      instance_types = ["t2.small"]
+    }
+  }
+}
